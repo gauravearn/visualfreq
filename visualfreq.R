81534667c4e855e2acb2f3a5b610b2f05c3f63e7
Gaurav Sablok <sablokg@gmail.com>
2024-04-17 22:11:37 +0200

    Update visualfreq.R

--- visualfreq.R
+++ visualfreq.R
@@ -4,7 +4,7 @@
 # Date 2024-3-21
 # a gene alignment to the visual alignment priting 
 # for the creation of the alignment maps
-visualfreq <- function(inputfile){
+visualfreq <- function(inputfile, alignmentstart, alignmentend){
 suppressPackageStartupMessages(library(argparser, pos = "package:base"))
 suppressPackageStartupMessages(library(methods, pos = "package:base"))
 suppressPackageStartupMessages(library(Biostrings, pos = "package:base"))
@@ -22,20 +22,18 @@ suppressPackageStartupMessages(library(reticulate, pos = "package:base"))
   alignmentwrite <- msaConvert(alignment, type = "ape::DNAbin")
   write.FASTA(alignmentwrite, file = "alignment.fasta")
   fasta <- "/home/gaurav/Desktop/arabidopsis/alignment.fasta"
-  ggmsa(fasta, 164, 213, color="Chemistry_AA")
+  ggmsa(fasta, alignmentstart, alignmentend, color="Chemistry_AA")
   complete_likelihood <- TreeLine(myXStringSet = 
-          readDNAStringSet(file = "alignment.fasta"),myDistMatrix = 
-         DistanceMatrix(RemoveGaps(StaggerAlignment(AlignSeqs(readDNAStringSet(
-                     file = "alignment.fasta"))), 
+             readDNAStringSet(file = "alignment.fasta"),myDistMatrix = 
+                        DistanceMatrix(RemoveGaps(StaggerAlignment(AlignSeqs(readDNAStringSet(file = "alignment.fasta"))), 
                            removeGaps = "all", processors = 3), type = "dist"), 
        method = "complete", cutoff = 0.05, showPlot = TRUE, reconstruct = TRUE)
   WriteDendrogram(complete_likelihood, file = "maximum_likelihood.txt")
-  ggmsa(DNAMultipleAlignment(msaMuscle(readDNAStringSet(
- file = "alignment.fasta"))), 
+  ggmsa(DNAMultipleAlignment(msaMuscle(readDNAStringSet(file = "alignment.fasta"))), 
  color = "Shapely_NT", font = "DroidSansMono", 
  char_width = 0.5,seq_name = TRUE + geom_seqlogo(color = "Shapely_NT")) 
   +geom_msaBar() + geom_GC()
-  ggsave(alignment_plot, width = 4, height = 4)
+  ggsave("alignment_plot.pdf")
   sink(file = "distance_matrix_arabiopsis.txt")
   dist.dna(read.FASTA(file = "alignment.fasta", type = "DNA"))
   sink()
